1ae72ab97df5eb27523fff13eeeae41f96f90df5
Max Henrik Balsmeier <mhbalsmeier@gmail.com>
2020-05-09 19:12:26 +0200

    .
    Former-commit-id: 43093f522900a2e8943123c5f84a441fe6e24e05

--- core/run_configs/test_1.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-run_span=1800
-write_out_interval=288
-grid_props_file=/home/max/my_code/game/grid_generator/nc_files/B4L6T30000_M2_O0.nc
-init_state_file=/home/max/my_code/game/test_generator/grib_files/test_1_B4L6T30000_M2_O0.grb2
-output_dir=output/test_1
-cfl_margin=0.2
-source run
