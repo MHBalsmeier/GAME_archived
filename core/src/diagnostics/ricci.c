66e514255db4449ab8c8158b070cb5524079b298
Max H. Balsmeier <mhbalsmeier@gmail.com>
2020-06-05 11:28:45 +0200

    Update ricci.c
    Former-commit-id: 6f497b57f1f6c47c6381dcf69bef6e5a65c4a7fb

--- core/src/diagnostics/ricci.c
+++ core/src/diagnostics/ricci.c
@@ -5,7 +5,23 @@ double inner_elementary(double [], double []);
 
 int vertical_contravariant_normalized(Vector_field in_field, int layer_index, int h_index, Grid *grid, double *result)
 {
-	*result = in_field[layer_index*NUMBER_OF_VECTORS_PER_LAYER + h_index];
+	double x_component, y_component;
+	int retval = recov_ver_pri_0(in_field, layer_index, h_index, &x_component, grid);
+	if (retval != 0)
+		printf("Error in recov_ver_pri_0 called at position 0 from horizontal_covariant_normalized.\n");
+	int retval = recov_ver_pri_1(in_field, layer_index, h_index, &y_component, grid);
+	if (retval != 0)
+		printf("Error in recov_ver_pri_1 called at position 0 from horizontal_covariant_normalized.\n");
+	int vector_index = layer_index*NUMBER_OF_VECTORS_PER_LAYER + h_index;
+	double velocity_vector[3];
+	velocity_vector[0] = x_component;
+	velocity_vector[1] = y_component;
+	velocity_vector[2] = in_field[vector_index];
+	double unit_vector[3];
+	unit_vector[0] = 0;
+	unit_vector[1] = 0;
+	unit_vector[2] = 1;
+	*result = inner_elementary(velocity_vector, unit_vector);
 	return 0;
 }
 
