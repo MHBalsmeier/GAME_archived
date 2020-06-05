585a04c1b9de2f77711c4516256339bc117fe8da
Max H. Balsmeier <mhbalsmeier@gmail.com>
2020-06-05 10:45:35 +0200

    Update ricci.c
    Former-commit-id: efc4b580a43925de8b6342c26ecd872c038524a1

--- core/src/diagnostics/ricci.c
+++ core/src/diagnostics/ricci.c
@@ -1,5 +1,5 @@
 #include "../enum_and_typedefs.h"
-#include <math.h>
+#include <stdio.h>
 
 double inner_elementary(double [], double []);
 
@@ -11,7 +11,23 @@ int vertical_contravariant_normalized(Vector_field in_field, int layer_index, in
 
 int horizontal_covariant_normalized(Vector_field in_field, int layer_index, int h_index, Grid *grid, double *result)
 {
-	*result = in_field[layer_index*NUMBER_OF_VECTORS_PER_LAYER + NUMBER_OF_VECTORS_V + h_index];
+	double vertical_component;
+	int retval = recov_hor_ver_pri(in_field, layer_index, h_index, &vertical_component, grid);
+	if (retval != 0)
+		printf("Error in recov_hor_ver_pri called at position 0 from horizontal_covariant_normalized.\n");
+	int vector_index = layer_index*NUMBER_OF_VECTORS_PER_LAYER + NUMBER_OF_VECTORS_V + h_index;
+	double delta_z = z_scalar[grid -> to_index[vector_index]] - z_scalar[grid -> from_index[vector_index]];
+	double delta_x = grid -> normal_distance[];
+	double angle = atan(delta_z, delta_x);
+	double velocity_vector[3];
+	velocity_vector[0] = in_field[vector_index];
+	velocity_vector[1] = 0;
+	velocity_vector[2] = vertical_component;
+	double unit_vector[3];
+	unit_vector[0] = cos(angle);
+	unit_vector[1] = 0;
+	unit_vector[2] = sin(angle);
+	*result = inner_elementary(velocity_vector, unit_vector);
 	return 0;
 }
 
