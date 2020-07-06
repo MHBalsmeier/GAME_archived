3922999287b620a25ca9ec47f1610c0278893930
Max Henrik Balsmeier <mhbalsmeier@gmail.com>
2020-07-06 23:15:59 +0200

    .
    Former-commit-id: 5683b98d2a08a11557a758b722070f0174a0e1af

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/max/my_code/game/core
-# Build directory: /home/max/my_code/game/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
