3c00e14f808a9bb443cc9c173bb13f9061576248
Max Henrik Balsmeier <mhbalsmeier@gmail.com>
2020-07-06 18:09:20 +0200

    Grid files moved.
    Former-commit-id: 29795fd6b9efb04c822d26136f32e4e6fceae924

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -2,7 +2,7 @@
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "/home/max/compiled/game_dev")
+  set(CMAKE_INSTALL_PREFIX "/home/max/compiled/game")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
