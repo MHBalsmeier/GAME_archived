acf41506cf60cc989d4b0a573591f5cdbbd8bb08
Max Henrik Balsmeier <mhbalsmeier@gmail.com>
2020-07-06 23:51:59 +0200

    .
    Former-commit-id: 367de1fc8b88bce7bd53767da9d72be43318b359

--- test_generator/compile.sh
+++ test_generator/compile.sh
@@ -1,1 +1,3 @@
+echo "Starting to compile test generator."
 gcc test_generator.c -leccodes -lnetcdf -lm -lgeos95 -latmostracers -Wl,-rpath=/lib -Wall -o test_generator
+echo "Test generator compiled succesfully."
