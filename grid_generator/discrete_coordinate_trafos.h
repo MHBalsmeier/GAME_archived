11639d8c9b323eb68dd0fcb94f10e38b165e9cb5
Max Henrik Balsmeier <mhbalsmeier@gmail.com>
2020-07-07 22:33:41 +0200

    Some changes.
    Former-commit-id: ee93019931258d572b82448e18f0d2e5035e22b0

---
+++ grid_generator/discrete_coordinate_trafos.h
@@ -0,0 +1,24 @@
+int find_triangle_on_face_index_from_dual_scalar_on_face_index(int, int, int *, int *, int *, int *);
+int find_triangle_edge_points_from_dual_scalar_on_face_index(int, int, int, int *, int *, int *, int [][3], int face_edges[][3], int face_edges_reverse[][3]);
+int find_points_per_edge(int, int *);
+int find_scalar_points_per_inner_face(int, int *);
+int upscale_scalar_point(int, int, int *);
+int write_scalar_coordinates(int, int, int, int, int, int, int, double [], double [], double [], double [], double []);
+int find_triangles_per_face(int, int *);
+int find_v_vector_indices_for_dual_scalar_z(int [], int [], int [], int, int []);
+int find_angle_change(double, double, double *);
+int find_coords_from_triangle_on_face_index(int, int, int *, int *, int *);
+int find_triangle_on_face_index_from_coords(int, int, int, int *);
+int find_triangle_indices_from_h_vector_index(int, int, int *, int *, int *, int *, int *, int *, int *, int *, int [][3], int [][3], int [][2], int [][3]);
+int find_triangle_edge_points(int, int, int, int *, int *, int *, int *, int *, int *, int *, int [][3], int [][3], int [][3]);
+
+
+
+
+
+
+
+
+
+
+
--- grid_generator/enum_and_typedefs.h
+++ grid_generator/enum_and_typedefs.h
@@ -16,58 +16,33 @@
 #define SEMIMAJOR 6378137.0
 #define SEMIMINOR 6356752.314
 
-typedef double Scalar_field[NUMBER_OF_SCALARS];
-typedef double Vector_field[NUMBER_OF_VECTORS];
-typedef double Dual_vector_field[NUMBER_OF_DUAL_VECTORS];
-
-typedef struct grid {
-Vector_field gravity;
-Vector_field normal_distance;
-Scalar_field volume;
-Vector_field area;
-Scalar_field z_scalar;
-Vector_field z_vector;
-long from_index[NUMBER_OF_VECTORS_H];
-long to_index[NUMBER_OF_VECTORS_H];
-double direction[NUMBER_OF_VECTORS_H];
-long adjacent_vector_indices_h[6*NUMBER_OF_SCALARS_H];
-short adjacent_signs_h[6*NUMBER_OF_SCALARS_H];
-long vorticity_indices[6*NUMBER_OF_VECTORS_V];
-short vorticity_signs[6*NUMBER_OF_VECTORS_V];
-long h_curl_indices[4*NUMBER_OF_VECTORS_H];
-short h_curl_signs[4*NUMBER_OF_VECTORS_H];
-long recov_hor_par_dual_index[2*NUMBER_OF_VECTORS_H];
-double recov_hor_par_dual_weight[2*NUMBER_OF_VECTORS_H];
-long recov_hor_ver_dual_index[2*NUMBER_OF_VECTORS_H];
-double recov_hor_ver_dual_weight[2*NUMBER_OF_VECTORS_H];
-long recov_hor_par_pri_index[4*NUMBER_OF_VECTORS_H];
-double recov_hor_par_pri_weight[4*NUMBER_OF_VECTORS_H];
-long recov_hor_ver_pri_index[4*NUMBER_OF_VECTORS_H];
-double recov_hor_ver_pri_weight[4*NUMBER_OF_VECTORS_H];
-long recov_ver_0_pri_index[12*NUMBER_OF_VECTORS_V];
-double recov_ver_0_pri_weight[12*NUMBER_OF_VECTORS_V];
-long recov_ver_0_dual_index[6*NUMBER_OF_VECTORS_V];
-double recov_ver_0_dual_weight[6*NUMBER_OF_VECTORS_V];
-long recov_ver_1_pri_index[12*NUMBER_OF_VECTORS_V];
-double recov_ver_1_pri_weight[12*NUMBER_OF_VECTORS_V];
-long recov_ver_1_dual_index[6*NUMBER_OF_VECTORS_V];
-double recov_ver_1_dual_weight[6*NUMBER_OF_VECTORS_V];
-} Grid;
-
-typedef struct dualgrid {
-Dual_vector_field normal_distance;
-Dual_vector_field area;
-double f_vec[NUMBER_OF_DUAL_VECTORS_PER_LAYER];
-long to_index[NUMBER_OF_DUAL_VECTORS_H];
-long from_index[NUMBER_OF_DUAL_VECTORS_H];
-long vorticity_indices[3*NUMBER_OF_DUAL_VECTORS_V];
-short vorticity_signs[3*NUMBER_OF_DUAL_VECTORS_V];
-long h_curl_indices[4*NUMBER_OF_DUAL_VECTORS_H];
-short h_curl_signs[4*NUMBER_OF_DUAL_VECTORS_H];
-} Dualgrid;
-
-typedef struct state {
-Scalar_field density_pot_temp;
-Scalar_field density;
-Vector_field wind;
-} State;
+enum grid_integers {
+RES_ID = 5,
+NUMBER_OF_BASIC_TRIANGLES = 20,
+NUMBER_OF_PENTAGONS = 12,
+NUMBER_OF_HEXAGONS = (int) (10*(pow(2, 2*RES_ID) - 1)),
+NUMBER_OF_EDGES = 3*NUMBER_OF_BASIC_TRIANGLES/2,
+NUMBER_OF_LAYERS = 26,
+NUMBER_OF_ORO_LAYERS = 17,
+NUMBER_OF_LEVELS = NUMBER_OF_LAYERS + 1,
+NUMBER_OF_SCALARS_H = NUMBER_OF_PENTAGONS + NUMBER_OF_HEXAGONS,
+NUMBER_OF_VECTORS_H = (5*NUMBER_OF_PENTAGONS/2 + 6/2*NUMBER_OF_HEXAGONS),
+NUMBER_OF_VECTORS_V = NUMBER_OF_SCALARS_H,
+NUMBER_OF_H_VECTORS = NUMBER_OF_LAYERS*NUMBER_OF_VECTORS_H,
+NUMBER_OF_V_VECTORS = NUMBER_OF_LEVELS*NUMBER_OF_VECTORS_V,
+NUMBER_OF_VECTORS_PER_LAYER = NUMBER_OF_VECTORS_H + NUMBER_OF_VECTORS_V,
+NUMBER_OF_TRIANGLES = (int) (NUMBER_OF_BASIC_TRIANGLES*(pow(4, RES_ID))),
+NUMBER_OF_SCALARS = NUMBER_OF_SCALARS_H*NUMBER_OF_LAYERS,
+NUMBER_OF_VECTORS = NUMBER_OF_H_VECTORS + NUMBER_OF_V_VECTORS,
+NUMBER_OF_DUAL_SCALARS_H = NUMBER_OF_TRIANGLES,
+NUMBER_OF_DUAL_VECTORS_H = 3*NUMBER_OF_TRIANGLES/2,
+NUMBER_OF_DUAL_VECTORS_V = NUMBER_OF_DUAL_SCALARS_H,
+NUMBER_OF_DUAL_H_VECTORS = NUMBER_OF_LEVELS*NUMBER_OF_DUAL_VECTORS_H,
+NUMBER_OF_DUAL_V_VECTORS = NUMBER_OF_LAYERS*NUMBER_OF_DUAL_VECTORS_V,
+NUMBER_OF_DUAL_VECTORS_PER_LAYER = NUMBER_OF_DUAL_VECTORS_H + NUMBER_OF_DUAL_VECTORS_V,
+NUMBER_OF_DUAL_SCALARS = NUMBER_OF_LEVELS*NUMBER_OF_DUAL_SCALARS_H,
+NUMBER_OF_DUAL_VECTORS = NUMBER_OF_DUAL_H_VECTORS + NUMBER_OF_DUAL_V_VECTORS,
+TRIANGLES_PER_FACE = NUMBER_OF_TRIANGLES/NUMBER_OF_BASIC_TRIANGLES,
+POINTS_PER_EDGE = (int) (pow(2, RES_ID) - 1),
+SCALAR_POINTS_PER_INNER_FACE = (int) (0.5*(pow(2, RES_ID) - 2)*(pow(2, RES_ID) - 1)),
+VECTOR_POINTS_PER_INNER_FACE = (int) (1.5*(pow(2, RES_ID) - 1)*pow(2, RES_ID))};
