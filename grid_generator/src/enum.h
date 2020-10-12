5ac4cad3179fa9e8e300d58856c42d38cddaf7d5
Max H. Balsmeier <mhbalsmeier@gnail.com>
2020-10-12 17:22:03 +0200

    minor

--- grid_generator/src/enum.h
+++ grid_generator/src/enum.h
@@ -1,3 +1,8 @@
+/*
+This source file is part of the Geophysical Fluids Modeling Framework (GAME), which is released under the MIT license.
+Github repository: https://github.com/MHBalsmeier/game
+*/
+
 #include <math.h>
 #define N_A (6.0221409e23)
 #define K_B (1.380649e-23)
--- orography_generator/src/enum.h
+++ orography_generator/src/enum.h
@@ -1,3 +1,8 @@
+/*
+This source file is part of the Geophysical Fluids Modeling Framework (GAME), which is released under the MIT license.
+Github repository: https://github.com/MHBalsmeier/game
+*/
+
 #include <math.h>
 
 enum grid_integers {
