40d95cd291167c44154aebfc7ab227e67dfa2243
Max H. Balsmeier <mhbalsmeier@gnail.com>
2020-10-13 12:44:10 +0200

    minor

--- bash_replace.sh
+++ bash_replace.sh
@@ -1,3 +1,10 @@
-old_string="Geophysical"
-new_string="Geophysical"
+#!/bin/bash
+
+# This source file is part of the Geophysical Fluids Modeling Framework (GAME), which is released under the MIT license.
+# Github repository: https://github.com/MHBalsmeier/game
+
+# This script can be used to replace strings in all files of the project.
+
+old_string="triangular"
+new_string="hexagonal"
 grep -rl $old_string . | xargs sed -i 's/'$old_string'/'$new_string'/g'
