8dea40e91ababa3f46426aecefc594d3b3958264
Max Henrik Balsmeier <mhbalsmeier@gmail.com>
2020-11-20 22:27:50 +0100

    Radiation extended.

--- core/src/physics/mo_radiation.f90
+++ core/src/physics/mo_radiation.f90
@@ -6,10 +6,21 @@ module radiation
   use iso_c_binding,
   use mo_rte_kind,           only: wp
   use mo_gas_optics_rrtmgp,  only: ty_gas_optics_rrtmgp
+  use mo_gas_concentrations, only: ty_gas_concs
+  use mo_fluxes_byband,      only: ty_fluxes_byband
   use mo_rte_sw,             only: rte_sw
   use mo_rte_lw,             only: rte_lw
   
   implicit none
+  
+  ! the number of bands in the short wave region
+  integer, parameter                 :: no_of_sw_bands = 14
+  ! the number of bands in the long wave region
+  integer, parameter                 :: no_of_lw_bands = 16
+  ! the number of g points is the short wave region
+  integer                            :: no_of_sw_g_points
+  ! the number of g points is the long wave region
+  integer                            :: no_of_lw_g_points
   
   type(ty_gas_optics_rrtmgp) :: k_dist_sw, k_dist_lw
 
@@ -41,15 +52,61 @@ module radiation
     ! local variables
     ! solar zenith angle
     real(8)	                         :: mu_0(no_of_scalars/no_of_layers)
-    integer                          :: ji, no_of_scalars_h
+    ! the gas concentrations
+    type(ty_gas_concs)               :: gas_concentrations
+    ! number of scalars per layer (number of columns)
+    integer                          :: no_of_scalars_h
+    ! number of points where it is day
+    integer                          :: no_day_points
+    ! number of points where it is night
+    integer                          :: no_night_points
+    ! loop indices
+    integer                          :: ji, j_day, j_night
+    ! the indices of columns where it is day
+    integer                          :: day_indices(no_of_scalars/no_of_layers)
+    ! the indices of columns where it is night
+    integer                          :: night_indices(no_of_scalars/no_of_layers)
+    ! the resulting clear sky fluxes
+    type(ty_fluxes_byband)           :: fluxes_clearsky_day
+    real(8)                          :: surface_emissivity(no_of_lw_bands, no_of_scalars/no_of_layers)
+    real(8)                          :: albedo_dir        (no_of_lw_bands, no_of_scalars/no_of_layers)
+    real(8)                          :: albedo_dif        (no_of_lw_bands, no_of_scalars/no_of_layers)
     
-    ! the number of scalars on every layer
+    ! calculation of the number of columns
     no_of_scalars_h = no_of_scalars/no_of_layers
     
-    ! calculating the zenith angle
+    ! set the surface emissivity to one
+    surface_emissivity(:,:) = 1.
+    
+    ! set the surface albedos to 0.5
+    albedo_dir        (:,:) = 0.5
+    albedo_dif        (:,:) = 0.5
+    
+    ! calculating the zenith angle, and counting day and night points
+    j_day = 0
+    j_night = 0
     do ji=1,no_of_scalars_h
       mu_0(ji) = coszenith(latitude_scalar(ji), longitude_scalar(ji), time_coord)
+      if (mu_0(ji) > 0) then
+        j_day   = j_day + 1
+        day_indices(j_day)     = ji
+      else
+        j_night = j_night + 1
+        night_indices(j_night) = ji
+      endif
     enddo
+    
+    no_day_points   = j_day
+    no_night_points = j_night
+    
+    no_of_sw_g_points = k_dist_sw%get_ngpt()
+    no_of_lw_g_points = k_dist_lw%get_ngpt()
+    
+    ! calculate shorwave radiative fluxes
+    ! rte_sw(k_dist_sw, gas_concentrations, albedo_dir, albedo_dif, mu_0)
+    
+    ! calculate longwave radiative fluxes
+    ! rte_lw(k_dist_lw, gas_concentrations, surface_emissivity(:,:))
     
     ! the result
     do ji=1,no_of_scalars
