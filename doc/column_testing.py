5687ce6a55377c0c9c872e52e4db487314dbbffe
Max Henrik Balsmeier <mhbalsmeier@gmail.com>
2020-11-22 14:09:05 +0100

    minor

--- doc/column_testing.py
+++ doc/column_testing.py
@@ -1,3 +1,4 @@
+
 import numpy as np;
 import math;
 import matplotlib.pyplot as plt;
@@ -11,11 +12,12 @@ no_of_layers = 100;
 # background T
 T_0 = 273.15;
 # time step
-delta_t = 0.1;
+delta_t = 1000;
 # number of steps you want to integrate
-no_of_steps = 2000;
+no_of_steps = 100;
 #name of the time integration scheme
 time_integration = "Euler explicit";
+time_integration = "implicit";
 # turn avection on or off
 adv = 0;
 # turn gravity on or off
@@ -24,6 +26,22 @@ grav_switch = 0;
 p_surf = 101325;
 
 ### END OF INPUT SECTION
+
+def thomas_algorithm(a_vector, b_vector, c_vector, d_vector, solution_length):
+	# https://en.wikipedia.org/wiki/Tridiagonal_matrix_algorithm
+	solution_vector = np.zeros([solution_length]);
+	c_prime_vector = np.zeros([solution_length - 1]);
+	d_prime_vector = np.zeros([solution_length]);
+	c_prime_vector[0] = c_vector[0]/b_vector[0];
+	for j in np.arange(1, solution_length - 1):
+		c_prime_vector[j] = c_vector[j]/(b_vector[j] - c_prime_vector[j - 1]*a_vector[j - 1]);
+	d_prime_vector[0] = d_vector[0]/b_vector[0];
+	for j in np.arange(1, solution_length):
+		d_prime_vector[j] = (d_vector[j] - d_prime_vector[j - 1]*a_vector[j - 1])/(b_vector[j] - c_prime_vector[j - 1]*a_vector[j - 1]);
+	solution_vector[solution_length - 1] = d_prime_vector[solution_length - 1];
+	for j in np.arange(solution_length - 2, -1, -1):
+		solution_vector[j] = d_prime_vector[j] - c_prime_vector[j]*solution_vector[j + 1];
+	return solution_vector;
 
 # constants:
 
@@ -66,8 +84,8 @@ rho_new = np.zeros([no_of_levels]);
 for i in range(no_of_layers):
 	T_old[i] = T_0;
 	# adding a small perurbation
-	if (i == 50):
-		T_old[i] = T_old[i] + 0.1;
+	if (i >= 50):
+		T_old[i] = T_old[i] + 100;
 	T_new[i] = T_old[i];
 # initializing the velocity field
 for i in range(no_of_levels):
@@ -87,10 +105,57 @@ for i in range(no_of_steps):
 	if (time_integration == "Euler explicit"):
 		for j in range(no_of_layers):
 			div_w = (w_old[j] - w_old[j + 1])/(z_levels[j] - z_levels[j + 1]);
-			T_new[j] = T_old[j] - delta_t*R_d*T_old[j]/c_v*div_w;
+			T_new[j] = T_old[j] - delta_t*R_d/c_v*T_old[j]*div_w;
 		for j in np.arange(1, no_of_levels - 1):
 			grad_T = (T_old[j - 1] - T_old[j])/(z_layers[j - 1] - z_layers[j]);
 			w_new[j] = w_old[j] - delta_t*c_p*grad_T;
+		for j in range(no_of_layers):
+			T_old[j] = T_new[j];
+		for j in range(no_of_levels):
+			w_old[j] = w_new[j];
+	if (time_integration == "implicit"):
+		solution_length = no_of_layers + no_of_levels;
+		a_vector = np.zeros([solution_length - 1]);
+		b_vector = np.zeros([solution_length]);
+		c_vector = np.zeros([solution_length - 1]);
+		d_vector = np.zeros([solution_length]);
+		for j in range(no_of_layers):
+			# this refers to the temperature
+			delta_z = z_levels[j] - z_levels[j + 1];
+			a_vector[2*j] = delta_t*R_d/c_v*T_old[j]/delta_z;
+			# this refers to the vertical velocity
+			if j == no_of_layers - 1:
+				a_vector[2*j + 1] = 0;
+			else:
+				delta_z = z_layers[j] - z_layers[j + 1];
+				a_vector[2*j + 1] = delta_t*c_p/delta_z;
+			# this refers to the vertical velocity
+			if j == 0:
+				c_vector[2*j] = 0;
+			else:
+				delta_z = z_layers[j - 1] - z_layers[j];
+				c_vector[2*j] = -delta_t*c_p/delta_z;
+			# this refers to the temperature
+			delta_z = z_levels[j] - z_levels[j + 1];
+			c_vector[2*j + 1] = -delta_t*R_d/c_v*T_old[j]/delta_z;
+		for j in range(no_of_layers):
+			# vertical velocity
+			b_vector[2*j] = 1;
+			# temperature
+			b_vector[2*j + 1] = 1;
+			# explicit component of vertical velocity
+			d_vector[2*j] = w_old[j];
+			# explicit component of temperature
+			d_vector[2*j + 1] = T_old[j];
+		b_vector[solution_length - 1] = 1;
+		# explicit component of vertical velocity at the surface
+		d_vector[solution_length - 1] = w_old[no_of_levels - 1];
+		solution_vector = np.zeros([solution_length]);
+		solution_vector = thomas_algorithm(a_vector, b_vector, c_vector, d_vector, solution_length);
+		for j in range(no_of_layers):
+			T_new[j] = solution_vector[2*j + 1];
+		for j in range(no_of_levels):
+			w_new[j] = solution_vector[2*j];
 		for j in range(no_of_layers):
 			T_old[j] = T_new[j];
 		for j in range(no_of_levels):
@@ -123,9 +188,6 @@ plt.plot(delta_t*np.arange(0, no_of_steps), (e_tot - e_tot[0])/e_tot[0]);
 plt.xlim([0, np.max(delta_t*np.arange(0, no_of_steps))]);
 plt.legend(["internal", "potential", "kinetic", "total"]);
 plt.show();
-
-
-
 
 
 
