fe604bbc3593af6d9afa2653e5ab391ced4d6ef3
Max Henrik Balsmeier <mhbalsmeier@gmail.com>
2020-11-25 00:56:02 +0100

    Gradient extrapolation generalized.

--- core/src/spatial_operators/gradient_operators.c
+++ core/src/spatial_operators/gradient_operators.c
@@ -12,6 +12,7 @@ int grad(Scalar_field in_field, Vector_field out_field, Grid *grid)
 {
     int layer_index, h_index, lower_index, upper_index;
     double vertical_gradient;
+    // loop over the inner grid points
 	#pragma omp parallel for private(layer_index, h_index, lower_index, upper_index, vertical_gradient)
     for (int i = NO_OF_SCALARS_H; i < NO_OF_VECTORS - NO_OF_SCALARS_H; ++i)
     {
@@ -28,16 +29,27 @@ int grad(Scalar_field in_field, Vector_field out_field, Grid *grid)
             out_field[i] = (in_field[upper_index] - in_field[lower_index])/grid -> normal_distance[i];
         }
     }
+    // linear extrapolation to the TOA
     #pragma omp parallel for
     for (int i = 0; i < NO_OF_SCALARS_H; ++i)
     {
-        out_field[i] = out_field[i + NO_OF_VECTORS_PER_LAYER] + out_field[i + NO_OF_VECTORS_PER_LAYER] - out_field[i + 2*NO_OF_VECTORS_PER_LAYER];
+        out_field[i] =
+        out_field[i + NO_OF_VECTORS_PER_LAYER] +
+        (out_field[i + NO_OF_VECTORS_PER_LAYER] - out_field[i + 2*NO_OF_VECTORS_PER_LAYER])/
+        (grid -> z_vector[i + NO_OF_VECTORS_PER_LAYER] - grid -> z_vector[i + 2*NO_OF_VECTORS_PER_LAYER])*
+        (grid -> z_vector[i] - grid -> z_vector[i + NO_OF_VECTORS_PER_LAYER]);
     }
+    // linear extrapolation to the surface
     #pragma omp parallel for
     for (int i = NO_OF_VECTORS - NO_OF_SCALARS_H; i < NO_OF_VECTORS; ++i)
     {
-        out_field[i] = out_field[i - NO_OF_VECTORS_PER_LAYER] + out_field[i - NO_OF_VECTORS_PER_LAYER] - out_field[i - 2*NO_OF_VECTORS_PER_LAYER];
+        out_field[i] =
+        out_field[i - NO_OF_VECTORS_PER_LAYER] +
+        (out_field[i - 2*NO_OF_VECTORS_PER_LAYER] - out_field[i - NO_OF_VECTORS_PER_LAYER])/
+        (grid -> z_vector[i - 2*NO_OF_VECTORS_PER_LAYER] - grid -> z_vector[i - NO_OF_VECTORS_PER_LAYER])
+        *(grid -> z_vector[i] - grid -> z_vector[i - NO_OF_VECTORS_PER_LAYER]);
     }
+    // correction for terrain
 	#pragma omp parallel for private(layer_index, h_index, vertical_gradient)
     for (int i = NO_OF_SCALARS_H; i < NO_OF_VECTORS - NO_OF_SCALARS_H; ++i)
     {
@@ -54,45 +66,7 @@ int grad(Scalar_field in_field, Vector_field out_field, Grid *grid)
 
 int scalar_times_grad(Scalar_field in_field_for_prefactor, Scalar_field in_field_for_grad, Vector_field out_field, Grid *grid)
 {
-    int layer_index, h_index, lower_index, upper_index;
-    double vertical_component;
-	#pragma omp parallel for private(layer_index, h_index, lower_index, upper_index, vertical_component)
-    for (int i = NO_OF_SCALARS_H; i < NO_OF_VECTORS - NO_OF_SCALARS_H; ++i)
-    {
-        layer_index = i/NO_OF_VECTORS_PER_LAYER;
-        h_index = i - layer_index*NO_OF_VECTORS_PER_LAYER;
-        if (h_index >= NO_OF_SCALARS_H)
-        {
-            out_field[i] = (in_field_for_grad[grid -> to_index[h_index - NO_OF_SCALARS_H] + layer_index*NO_OF_SCALARS_H] - in_field_for_grad[grid -> from_index[h_index - NO_OF_SCALARS_H] + layer_index*NO_OF_SCALARS_H])/grid -> normal_distance[i];
-        }
-        else
-        {
-            lower_index = h_index + layer_index*NO_OF_SCALARS_H;
-            upper_index = h_index + (layer_index - 1)*NO_OF_SCALARS_H;
-            out_field[i] = (in_field_for_grad[upper_index] - in_field_for_grad[lower_index])/grid -> normal_distance[i];
-        }
-    }
-    #pragma omp parallel for
-    for (int i = 0; i < NO_OF_SCALARS_H; ++i)
-    {
-        out_field[i] = out_field[i + NO_OF_VECTORS_PER_LAYER] + out_field[i + NO_OF_VECTORS_PER_LAYER] - out_field[i + 2*NO_OF_VECTORS_PER_LAYER];
-    }
-    #pragma omp parallel for
-    for (int i = NO_OF_VECTORS - NO_OF_SCALARS_H; i < NO_OF_VECTORS; ++i)
-    {
-        out_field[i] = out_field[i - NO_OF_VECTORS_PER_LAYER] + out_field[i - NO_OF_VECTORS_PER_LAYER] - out_field[i - 2*NO_OF_VECTORS_PER_LAYER];
-    }
-	#pragma omp parallel for private(layer_index, h_index, vertical_component)
-    for (int i = NO_OF_SCALARS_H; i < NO_OF_VECTORS - NO_OF_SCALARS_H; ++i)
-    {
-        layer_index = i/NO_OF_VECTORS_PER_LAYER;
-        h_index = i - layer_index*NO_OF_VECTORS_PER_LAYER;
-        if (h_index >= NO_OF_SCALARS_H && layer_index >= NO_OF_LAYERS - NO_OF_ORO_LAYERS)
-        {
-        	recov_hor_ver_pri(out_field, layer_index, h_index - NO_OF_SCALARS_H, &vertical_component, grid);
-            out_field[i] = out_field[i] - grid -> slope[i]*vertical_component;
-        }
-    }
+    grad(in_field_for_grad, out_field, grid);
 	scalar_times_vector(in_field_for_prefactor, out_field, out_field, grid, 0);
     return 0;
 }
