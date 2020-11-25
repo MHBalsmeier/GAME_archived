cc8923a85277ef2782f8fdd4713fdac3e548122b
Max Henrik Balsmeier <mhbalsmeier@gmail.com>
2020-11-25 01:31:07 +0100

    minor

--- run_scripts/standard_steep.sh
+++ run_scripts/standard_steep.sh
@@ -8,7 +8,7 @@ game_home_dir=/home/max/compiled/game_dev # the root directory of your GAME inst
 operator=MHB # the ID of the person / group / institution running the model
 ideal_input_id=1 # specifies which test scenario to run
 run_id=standard_steep # run_id must only be set if ideal_input_id != -1 (otherwise it is chosen automatically)
-run_span=7776000 # how long the model is supposed to run
+run_span=777600 # how long the model is supposed to run
 valgrind_check=0 # set this to 1, if you want to check the code with Valgrind
 
 # grid properties
