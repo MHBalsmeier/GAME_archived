0bbca27f1c00bcbc50accc6f59270a51a91662c3
Max Henrik Balsmeier <mhbalsmeier@gmail.com>
2020-11-28 00:13:25 +0100

    Minor.

---
+++ core/src/diagnostics/remap_verpri2horpri_vector.c
@@ -0,0 +1,20 @@
+/*
+This source file is part of the Geophysical Fluids Modeling Framework (GAME), which is released under the MIT license.
+Github repository: https://github.com/MHBalsmeier/game
+*/
+
+/*
+This function remaps vertical primal vectors to horizontal primal vector points.
+*/
+
+#include "../enum_and_typedefs.h"
+#include <stdio.h>
+
+int remap_verpri2horpri_vector(Vector_field in_field, int layer_index, int h_index, double *component, Grid *grid)
+{
+    *component = 0.5*grid -> volume_ratios[2*(layer_index*NO_OF_SCALARS_H + grid -> from_index[h_index]) + 0]*in_field[layer_index*NO_OF_VECTORS_PER_LAYER + grid -> from_index[h_index]];
+    *component += 0.5*grid -> volume_ratios[2*(layer_index*NO_OF_SCALARS_H + grid -> from_index[h_index]) + 1]*in_field[(layer_index + 1)*NO_OF_VECTORS_PER_LAYER + grid -> from_index[h_index]];
+    *component += 0.5*grid -> volume_ratios[2*(layer_index*NO_OF_SCALARS_H + grid -> to_index[h_index]) + 0]*in_field[layer_index*NO_OF_VECTORS_PER_LAYER + grid -> to_index[h_index]];
+    *component += 0.5*grid -> volume_ratios[2*(layer_index*NO_OF_SCALARS_H + grid -> to_index[h_index]) + 1]*in_field[(layer_index + 1)*NO_OF_VECTORS_PER_LAYER + grid -> to_index[h_index]];
+    return 0;
+}
---
+++ core/src/diagnostics/tangential_wind.c
@@ -0,0 +1,16 @@
+/*
+This source file is part of the Geophysical Fluids Modeling Framework (GAME), which is released under the MIT license.
+Github repository: https://github.com/MHBalsmeier/game
+*/
+
+#include "../enum_and_typedefs.h"
+
+int tangential_wind(Vector_field in_field, int layer_index, int h_index, double *component, Grid *grid)
+{
+    *component = 0;
+	for (int i = 0; i < 10; ++i)
+	{
+		*component += grid -> trsk_modified_weights[10*h_index + i]*in_field[NO_OF_SCALARS_H + layer_index*NO_OF_VECTORS_PER_LAYER + grid -> trsk_modified_velocity_indices[10*h_index + i]];
+	}
+    return 0;
+}
