bd7ee47646f0d5253438e10d68501e4b4f3f6490
Max Henrik Balsmeier <mhbalsmeier@gmail.com>
2021-02-13 23:29:01 +0100

    Maps script updated.

--- plotting/plot_maps_batch.sh
+++ plotting/plot_maps_batch.sh
@@ -16,66 +16,66 @@ grid_props_file=$game_home_dir/grids/B5L26T41152_O3_OL23_SCVT.nc # the file wher
 disp_shortname_list=(
 2t rprate sprate gust prmsl gh gh cape tcc
 2t rprate sprate gust prmsl gh gh cape tcc
-# 2t rprate sprate gust prmsl gh gh cape tcc
 2t rprate sprate gust prmsl gh gh cape tcc
 2t rprate sprate gust prmsl gh gh cape tcc
-# 2t rprate sprate gust prmsl gh gh cape tcc
+2t rprate sprate gust prmsl gh gh cape tcc
+2t rprate sprate gust prmsl gh gh cape tcc
 ) # short names according to grib as an array 
 disp_level_list=(
 2 0 0 10 0 500 200 0 0
 2 0 0 10 0 500 200 0 0
-# 2 0 0 10 0 500 200 0 0
 2 0 0 10 0 500 200 0 0
 2 0 0 10 0 500 200 0 0
-# 2 0 0 10 0 500 200 0 0
+2 0 0 10 0 500 200 0 0
+2 0 0 10 0 500 200 0 0
 ) # levels according to grib as an array
 on_pressure_level_list=(
 0 0 0 0 0 1 1 0 0
 0 0 0 0 0 1 1 0 0
-# 0 0 0 0 0 1 1 0 0
 0 0 0 0 0 1 1 0 0
 0 0 0 0 0 1 1 0 0
-# 0 0 0 0 0 1 1 0 0
+0 0 0 0 0 1 1 0 0
+0 0 0 0 0 1 1 0 0
 ) # set this to 1 for each plot individually if the variable resides on pressure levels
 plot_intervals_list=(
-21600 21600 21600 21600 21600 21600 21600 21600 21600
-21600 21600 21600 21600 21600 21600 21600 21600 21600
-# 21600 21600 21600 21600 21600 21600 21600 21600 21600
-21600 21600 21600 21600 21600 21600 21600 21600 21600
-21600 21600 21600 21600 21600 21600 21600 21600 21600
-# 21600 21600 21600 21600 21600 21600 21600 21600 21600
+3600 3600 3600 3600 3600 3600 3600 3600 3600
+3600 3600 3600 3600 3600 3600 3600 3600 3600
+3600 3600 3600 3600 3600 3600 3600 3600 3600
+3600 3600 3600 3600 3600 3600 3600 3600 3600
+3600 3600 3600 3600 3600 3600 3600 3600 3600
+3600 3600 3600 3600 3600 3600 3600 3600 3600
 ) # every how many seconds you want to plot each variable
 uniform_colormap_list=(
 0 0 0 0 0 0 0 0 0
 0 0 0 0 0 0 0 0 0
-# 0 0 0 0 0 0 0 0 0
 0 0 0 0 0 0 0 0 0
 0 0 0 0 0 0 0 0 0
-# 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0
 ) # set this to 1 for each plot individually if you want to enforce a uniform colormap for all the time steps
 scope_list=(
 CONUS CONUS CONUS CONUS CONUS CONUS CONUS CONUS CONUS
 CEU CEU CEU CEU CEU CEU CEU CEU CEU
-# CHINA CHINA CHINA CHINA CHINA CHINA CHINA CHINA CHINA
+CHINA CHINA CHINA CHINA CHINA CHINA CHINA CHINA CHINA
 INDIA INDIA INDIA INDIA INDIA INDIA INDIA INDIA INDIA
 CARIB CARIB CARIB CARIB CARIB CARIB CARIB CARIB CARIB
-# OCEAN OCEAN OCEAN OCEAN OCEAN OCEAN OCEAN OCEAN OCEAN
+OCEAN OCEAN OCEAN OCEAN OCEAN OCEAN OCEAN OCEAN OCEAN
 ) # the areas of the plots
 projections_list=(
 Gnomonic Gnomonic Gnomonic Gnomonic Gnomonic Gnomonic Gnomonic Gnomonic Gnomonic
 Gnomonic Gnomonic Gnomonic Gnomonic Gnomonic Gnomonic Gnomonic Gnomonic Gnomonic
-# Gnomonic Gnomonic Gnomonic Gnomonic Gnomonic Gnomonic Gnomonic Gnomonic Gnomonic
 Gnomonic Gnomonic Gnomonic Gnomonic Gnomonic Gnomonic Gnomonic Gnomonic Gnomonic
 Gnomonic Gnomonic Gnomonic Gnomonic Gnomonic Gnomonic Gnomonic Gnomonic Gnomonic
-# Gnomonic Gnomonic Gnomonic Gnomonic Gnomonic Gnomonic Gnomonic Gnomonic Gnomonic
+Gnomonic Gnomonic Gnomonic Gnomonic Gnomonic Gnomonic Gnomonic Gnomonic Gnomonic
+Gnomonic Gnomonic Gnomonic Gnomonic Gnomonic Gnomonic Gnomonic Gnomonic Gnomonic
 ) # the projections of the plots
 synoptical_time_mode=(
 1 1 1 1 1 1 1 1 1
 1 1 1 1 1 1 1 1 1
-# 1 1 1 1 1 1 1 1 1
 1 1 1 1 1 1 1 1 1
 1 1 1 1 1 1 1 1 1
-# 1 1 1 1 1 1 1 1 1
+1 1 1 1 1 1 1 1 1
+1 1 1 1 1 1 1 1 1
 ) # this forces the time description to be of the form "init: ..., valid: ... (+ ....)"
 source $game_home_dir/plotting/.sh/maps_root.sh # this is the script from which the python plot scripts are called
 
