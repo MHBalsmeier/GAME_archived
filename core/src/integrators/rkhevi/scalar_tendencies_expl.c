47aa22d65f7ab065c655a77fd9acdb42f43ee6eb
Max Henrik Balsmeier <mhbalsmeier@gmail.com>
2021-03-06 18:03:22 +0100

    RK2 weights changed in scalar tendencies.

--- core/src/integrators/rkhevi/scalar_tendencies_expl.c
+++ core/src/integrators/rkhevi/scalar_tendencies_expl.c
@@ -34,7 +34,7 @@ int scalar_tendencies_expl(State *state, State *state_tendency, Grid *grid, Dual
     new_weight = 1;
     if (no_rk_step == 1)
     {
-    	new_weight = 1;
+    	new_weight = 0.5;
     }
 	old_weight = 1 - new_weight;
 	
