d3616d2060d3642e4c56860b1a4ad986a9969902
Max Henrik Balsmeier <mhbalsmeier@gmail.com>
2021-03-06 23:27:41 +0100

    Minor.

--- core/src/spatial_operators/inner_product.c
+++ core/src/spatial_operators/inner_product.c
@@ -82,7 +82,7 @@ int curl_field_to_cells(Curl_field in_field, Scalar_field out_field, Grid *grid)
 
 int edges_to_cells(Vector_field in_field, Scalar_field out_field, Grid *grid)
 {
-	// This function averages a curl field from edges to centers.
+	// This function averages a vector field from edges to centers.
 	int layer_index, h_index, j, no_of_edges;
 	#pragma omp parallel for private (j, layer_index, h_index, no_of_edges)
     for (int i = 0; i < NO_OF_SCALARS; ++i)
@@ -105,22 +105,20 @@ int edges_to_cells(Vector_field in_field, Scalar_field out_field, Grid *grid)
 
 int edges_to_cells_lowest_layer(double in_field[NO_OF_VECTORS_H], double out_field[NO_OF_SCALARS_H], Grid *grid)
 {
-	// This function averages a curl field from edges to centers in the lowest layer.
-	int layer_index, h_index, j, no_of_edges;
-	#pragma omp parallel for private (j, layer_index, h_index, no_of_edges)
+	// This function averages a horizontal vector field (defined on a single layer) from edges to centers in the lowest layer.
+	int j, no_of_edges;
+	#pragma omp parallel for private (j, no_of_edges)
     for (int i = 0; i < NO_OF_SCALARS_H; ++i)
     {
-    	layer_index = i/NO_OF_SCALARS_H;
-    	h_index = i - layer_index*NO_OF_SCALARS_H;
         out_field[i] = 0;
         no_of_edges = 6;
-        if (h_index < NO_OF_PENTAGONS)
+        if (i < NO_OF_PENTAGONS)
         {
         	no_of_edges = 5;
         }
         for (j = 0; j < no_of_edges; ++j)
         {
-        	out_field[i] += 0.5*grid -> inner_product_weights[8*(NO_OF_SCALARS - NO_OF_SCALARS_H + i) + j]*in_field[grid -> adjacent_vector_indices_h[6*h_index + j]];
+        	out_field[i] += 0.5*grid -> inner_product_weights[8*(NO_OF_SCALARS - NO_OF_SCALARS_H + i) + j]*in_field[grid -> adjacent_vector_indices_h[6*i + j]];
     	}
     }
     return 0;
