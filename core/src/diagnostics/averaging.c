d899ff8d714379d9655e616b1149f78e12dbb82d
Max Henrik Balsmeier <mhbalsmeier@gmail.com>
2021-03-22 13:28:23 +0100

    Averaging modified.

--- core/src/diagnostics/averaging.c
+++ core/src/diagnostics/averaging.c
@@ -17,17 +17,23 @@ int remap_verpri2horpri_vector(Vector_field vector_field, int layer_index, int h
 	Reconstructs the certical vector component *component at edge h_index in layer layer_index.
 	*/
     *component
+    // layer above
     = grid -> inner_product_weights[8*(layer_index*NO_OF_SCALARS_H + grid -> from_index[h_index]) + 6]
     *vector_field[layer_index*NO_OF_VECTORS_PER_LAYER + grid -> from_index[h_index]];
     *component
-    += grid -> inner_product_weights[8*(layer_index*NO_OF_SCALARS_H + grid -> from_index[h_index]) + 7]
-    *vector_field[(layer_index + 1)*NO_OF_VECTORS_PER_LAYER + grid -> from_index[h_index]];
-    *component
     += grid -> inner_product_weights[8*(layer_index*NO_OF_SCALARS_H + grid -> to_index[h_index]) + 6]
     *vector_field[layer_index*NO_OF_VECTORS_PER_LAYER + grid -> to_index[h_index]];
-    *component
-    += grid -> inner_product_weights[8*(layer_index*NO_OF_SCALARS_H + grid -> to_index[h_index]) + 7]
-    *vector_field[(layer_index + 1)*NO_OF_VECTORS_PER_LAYER + grid -> to_index[h_index]];
+	// layer below
+    if (layer_index < NO_OF_LAYERS - 1)
+    {
+		*component
+		+= grid -> inner_product_weights[8*(layer_index*NO_OF_SCALARS_H + grid -> from_index[h_index]) + 7]
+		*vector_field[(layer_index + 1)*NO_OF_VECTORS_PER_LAYER + grid -> from_index[h_index]];
+		*component
+		+= grid -> inner_product_weights[8*(layer_index*NO_OF_SCALARS_H + grid -> to_index[h_index]) + 7]
+		*vector_field[(layer_index + 1)*NO_OF_VECTORS_PER_LAYER + grid -> to_index[h_index]];
+    }
+	// horizontal average
     *component = 0.5*(*component);
     return 0;
 }
