57a50125618456aec83c4eba93efda8cc4f5263b
Max H. Balsmeier <mhbalsmeier@gmail.com>
2021-03-30 00:21:53 +0200

    minor

--- plotting/plot_integrals.sh
+++ plotting/plot_integrals.sh
@@ -11,5 +11,5 @@ plot_mass_dry_integral=0 # set this to one if you want to plot the dry mass of t
 plot_entropy_gas_integral=0 # set this to one if you want to plot the entropy of the atmospheric domain
 plot_energy_integral=1 # set this to one if you want to plot the energy forms of the atmospheric domain
 plot_linearized_entropy_gas_integral=0 # set this to one if you want to plot the "linearized entropy" (c_p*density*log(potential temperature)) of the atmospheric domain
-delta_t=310 # the time step of the integration to plot
+delta_t=284 # the time step of the integration to plot
 source $game_home_dir/plotting/.sh/integrals_root.sh # this is the script from which the python plot scripts are called
