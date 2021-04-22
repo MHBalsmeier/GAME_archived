4c11e04ea6002efae70143cfe385d6e29406cf8b
Max Henrik Balsmeier <mhbalsmeier@gmail.com>
2021-04-23 00:27:22 +0200

    Surface components structurally included.

---
+++ core/src/soil/soil.c
@@ -0,0 +1,8 @@
+/*
+This source file is part of the Geophysical Fluids Modeling Framework (GAME), which is released under the MIT license.
+Github repository: https://github.com/AUN4GFD/game
+*/
+
+/*
+This function contains the soil component of GAME.
+*/
---
+++ core/src/waves_coupler/waves_coupler.c
@@ -0,0 +1,8 @@
+/*
+This source file is part of the Geophysical Fluids Modeling Framework (GAME), which is released under the MIT license.
+Github repository: https://github.com/AUN4GFD/game
+*/
+
+/*
+This function couples the atmospheric dynamical core of GAME to the internal or an external wave model.
+*/
