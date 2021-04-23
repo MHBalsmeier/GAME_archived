c6e0287c97a1a6d926e753d40fe55522724d4f35
Max Henrik Balsmeier <mhbalsmeier@gmail.com>
2021-04-24 00:03:33 +0200

    comment changed

--- core/src/time_stepping/manage_pressure_gradient.c
+++ core/src/time_stepping/manage_pressure_gradient.c
@@ -1,7 +1,6 @@
 /*
 This source file is part of the Geophysical Fluids Modeling Framework (GAME), which is released under the MIT license.
 Github repository: https://github.com/AUN4GFD/game
-The vertical advection of horizontal momentum is organized here.
 */
 
 #include "../enum_and_typedefs.h"
