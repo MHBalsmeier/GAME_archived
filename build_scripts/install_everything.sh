b5b98e91f4f89c9639a99f77e358c8b1e80782b2
Max Henrik Balsmeier <mhbalsmeier@gmail.com>
2021-08-12 01:29:31 +0200

    unnecessary install scripts removed

--- build_scripts/install_everything.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# This source file is part of the Geophysical Fluids Modeling Framework (GAME), which is released under the MIT license.
-# Github repository: https://github.com/AUN4GFD/game
-
-./install_grids.sh
-./install_tests.sh
-./install_run_scripts.sh
-./install_plotting_routines.sh
--- build_scripts/install_everything_dev.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# This source file is part of the Geophysical Fluids Modeling Framework (GAME), which is released under the MIT license.
-# Github repository: https://github.com/AUN4GFD/game
-
-./install_grids_dev.sh
-./install_tests_dev.sh
-./install_run_scripts_dev.sh
-./install_plotting_routines_dev.sh
