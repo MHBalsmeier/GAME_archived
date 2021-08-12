11cffd0e555ebcf8e57a4043430b355598d7bdc9
Max Henrik Balsmeier <mhbalsmeier@gmail.com>
2021-08-12 10:11:48 +0200

    comments changed

--- clean.sh
+++ clean.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# This source file is part of the Regional Forecasting with Poisson-brackets in Exner-Theta formulation (RFPET), which is released under the MIT license.
+# Github repository: https://github.com/OpenNWP/GAME
+
 if [ -d build ]
 then
 rm -r build/*
