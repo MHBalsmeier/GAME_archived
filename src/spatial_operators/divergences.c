fd8a468503995a5d5fd4e6f9f5f841821b5265b5
Max Henrik Balsmeier <mhbalsmeier@gmail.com>
2021-08-15 01:53:30 +0200

    minor bugfix

--- src/spatial_operators/divergences.c
+++ src/spatial_operators/divergences.c
@@ -37,7 +37,7 @@ int divv_h(Vector_field in_field, Scalar_field out_field, Grid *grid)
         if (layer_index == NO_OF_LAYERS - grid -> no_of_oro_layers - 1)
         {
             vertical_contravariant_corr(in_field, layer_index + 1, h_index, grid, &contra_lower);
-            comp_v = contra_lower*grid -> area[h_index + (layer_index + 1)*NO_OF_VECTORS_PER_LAYER];
+            comp_v = -contra_lower*grid -> area[h_index + (layer_index + 1)*NO_OF_VECTORS_PER_LAYER];
         }
         else if (layer_index == NO_OF_LAYERS - 1)
         {
