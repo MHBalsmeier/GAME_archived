0edb77ad5082ddbcaa7145829f3ced2393c252fe
Max H. Balsmeier <mhbalsmeier@gmail.com>
2021-08-16 00:38:48 +0200

    orography generator run script changed

--- orography_generator/run_script.sh
+++ orography_generator/run_script.sh
@@ -3,13 +3,13 @@
 # This source file is part of the Geophysical Fluids Modeling Framework (GAME), which is released under the MIT license.
 # Github repository: https://github.com/OpenNWP/GAME
 
-# oroid		description
+# oro_id	description
 # 1			Gaussian mountain at 0 N / 0 E, H = 10 km
 # 2			real data interpolated to model grid
 # See handbook for more information.
 
 oro_id=2 # The orography ID.
-rescale_factor=1.0 # rescale factor for the orography
+rescale_factor=0.6 # rescale factor for the orography (0.6 is sufficient to stabilize the model in real orography (oro_id=2))
 valgrind_check=0 # set this to 1, if you want to check the code with Valgrind
 export OMP_NUM_THREADS=1 # relevant only for OMP
 number_of_cpus=1 # relevant only for MPI
