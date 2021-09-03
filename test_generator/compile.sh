e6f6aa36db8d9459bced98c1ee80aff374bab2c3
Max Henrik Balsmeier <mhbalsmeier@gmail.com>
2021-09-04 00:13:47 +0200

    config change

--- test_generator/compile.sh
+++ test_generator/compile.sh
@@ -6,7 +6,7 @@
 dcmip_home_dir=../../DCMIP2016 # set this to the directory of the DCMIP2016 repository
 
 echo "Starting to compile test generator."
-mpicc src/test_generator.c ../src/spatial_operators/inner_product.c ../src/spatial_operators/gradient_operators.c ../src/spatial_operators/multiplications.c ../src/spatial_operators/vorticities.c ../src/spatial_operators/vorticity_flux.c ../src/spatial_operators/averaging.c ../src/thermodynamics/thermodynamics.c ../src/settings.c ../src/io/set_grid_props_and_dt.c $dcmip_home_dir/interface/baroclinic_wave_test.f90 ../shared/shared.c -leccodes -lnetcdf -lm -lgeos95 -fopenmp -latmostracers -Wl,-rpath=/lib -Wall -o test_generator
+gcc src/test_generator.c ../src/spatial_operators/inner_product.c ../src/spatial_operators/gradient_operators.c ../src/spatial_operators/multiplications.c ../src/spatial_operators/vorticities.c ../src/spatial_operators/vorticity_flux.c ../src/spatial_operators/averaging.c ../src/thermodynamics/thermodynamics.c ../src/settings.c ../src/io/set_grid_props_and_dt.c $dcmip_home_dir/interface/baroclinic_wave_test.f90 ../shared/shared.c -leccodes -lnetcdf -lm -lgeos95 -fopenmp -latmostracers -Wall -Wno-unused-dummy-argument -o test_generator
 if [ $? -ne 0 ]
 then
 echo -e ${RED}Test generator compilation failed.$NC
