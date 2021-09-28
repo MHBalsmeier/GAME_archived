583c5910423db57a46caeb62b75152bd4217b75e
Max Henrik Balsmeier <mhbalsmeier@gmail.com>
2021-09-28 14:25:42 +0200

    efficiency increased

--- src/spatial_operators/inner_product.c
+++ src/spatial_operators/inner_product.c
@@ -16,8 +16,8 @@ int inner_product(Vector_field in_field_0, Vector_field in_field_1, Scalar_field
     This function computes the inner product of the two vector fields in_field_0 and in_field_1. This is needed for computing the dissipation due to momentum diffusion (friction).
     */
     
-    int i, no_of_edges;
-    #pragma omp parallel for private (i, no_of_edges)
+    int i, no_of_edges, base_index;
+    #pragma omp parallel for private (i, no_of_edges, base_index)
 	for (int h_index = 0; h_index < NO_OF_SCALARS_H; ++h_index)
 	{
 	    no_of_edges = 6;
@@ -28,14 +28,15 @@ int inner_product(Vector_field in_field_0, Vector_field in_field_1, Scalar_field
 		for (int layer_index = 0; layer_index < NO_OF_LAYERS; ++layer_index)
 		{
 			i = layer_index*NO_OF_SCALARS_H + h_index;
+			base_index = 8*i;
 			out_field[i] = 0;
 			for (int j = 0; j < no_of_edges; ++j)
 			{
-			    out_field[i] += grid -> inner_product_weights[8*i + j]*in_field_0[NO_OF_SCALARS_H + layer_index*NO_OF_VECTORS_PER_LAYER + grid -> adjacent_vector_indices_h[6*h_index + j]]*
+			    out_field[i] += grid -> inner_product_weights[base_index + j]*in_field_0[NO_OF_SCALARS_H + layer_index*NO_OF_VECTORS_PER_LAYER + grid -> adjacent_vector_indices_h[6*h_index + j]]*
 				in_field_1[NO_OF_SCALARS_H + layer_index*NO_OF_VECTORS_PER_LAYER + grid -> adjacent_vector_indices_h[6*h_index + j]];
 			}
-			out_field[i] += grid -> inner_product_weights[8*i + 6]*in_field_0[h_index + layer_index*NO_OF_VECTORS_PER_LAYER]*in_field_1[h_index + layer_index*NO_OF_VECTORS_PER_LAYER];
-			out_field[i] += grid -> inner_product_weights[8*i + 7]*in_field_0[h_index + (layer_index + 1)*NO_OF_VECTORS_PER_LAYER]*in_field_1[h_index + (layer_index + 1)*NO_OF_VECTORS_PER_LAYER];
+			out_field[i] += grid -> inner_product_weights[base_index + 6]*in_field_0[h_index + layer_index*NO_OF_VECTORS_PER_LAYER]*in_field_1[h_index + layer_index*NO_OF_VECTORS_PER_LAYER];
+			out_field[i] += grid -> inner_product_weights[base_index + 7]*in_field_0[h_index + (layer_index + 1)*NO_OF_VECTORS_PER_LAYER]*in_field_1[h_index + (layer_index + 1)*NO_OF_VECTORS_PER_LAYER];
 		}
 	}
     return 0;
