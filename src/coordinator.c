a0210c94fa18e272876c0d7377da389d4b276813
Max Henrik Balsmeier <mhbalsmeier@gmail.com>
2021-09-29 11:08:46 +0200

    Minor.

--- src/coordinator.c
+++ src/coordinator.c
@@ -16,6 +16,7 @@ The main organizes the model, manages the time stepping, calls model output, col
 #include "io/io.h"
 #include "spatial_operators/spatial_operators.h"
 #include "radiation/radiation.h"
+#include "thermodynamics.h"
 #include "time_stepping/time_stepping.h"
 #include "soil.h"
 
