351705d3fa421807e8d9d98cb4cbc0c0151751e8
Max Henrik Balsmeier <mhbalsmeier@gmail.com>
2022-01-09 01:39:20 +0100

    run script of grid generator modified

--- grid_generator/run_script.sh
+++ grid_generator/run_script.sh
@@ -19,7 +19,7 @@ stretching_parameter=1.3 # stretching parameter of the vertical grid, must be >=
 orography_layers=23 # number of layers following orography (only relevant if type_of_vertical_grid == 0)
 toa=41152 # height of the top of the atmosphere
 type_of_vertical_grid=0 # 0: terrain following coordinates, 1: block-like orography
-valgrind_check=0 # set this to 1, if you want to check the code with Valgrind
+valgrind_check=0 # set this to 1 if you want to check the code with Valgrind
 number_of_cpus=1 # relevant only for MPI
 export OMP_NUM_THREADS=6 # relevant only for OMP
 source .sh/run.sh
