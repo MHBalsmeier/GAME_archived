352ecd374adc169b91f9439b4950e7b418b0578a
Max Henrik Balsmeier <mhbalsmeier@gmail.com>
2022-01-13 02:40:12 +0100

    bugfix

--- grid_generator/.sh/run.sh
+++ grid_generator/.sh/run.sh
@@ -25,14 +25,14 @@ echo "model top: "$toa" m"
 echo "type of vertical grid: "$type_of_vertical_grid
 # end verbosity
 
-if [ $oro_id -eq 2 ] && [ ! -f phys_quantities/B${res_id}_is_land.nc ]
-then
-
+# downloading orography if necessary
 if [ ! -f phys_quantities/etopo.nc ]
 then
 ./phys_quantities/download_etopo.sh
 fi
 
+if [ $oro_id -eq 2 ] && [ ! -f phys_quantities/B${res_id}_is_land.nc ]
+then
 echo "Creating land sea mask ..."
 python3 .py/is_land.py $res_id
 echo "Land sea mask created."
