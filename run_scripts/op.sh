be70a0529856f6f126888ceaa265fc1047cd649f
Max H. Balsmeier <mhbalsmeier@gmail.com>
2022-01-13 11:07:08 +0100

    bugfix in op run script

--- run_scripts/op.sh
+++ run_scripts/op.sh
@@ -18,18 +18,18 @@
 # -1:   NWP run
 
 # basic run properties
-game_home_dir=${BASH_ARGV[6]}
+game_home_dir=${BASH_ARGV[4]}
 ideal_input_id=-1 # specifies which test scenario to run (-1 corresponds to an NWP run)
-run_span=${BASH_ARGV[7]} # how long the model is supposed to run
-run_id=${BASH_ARGV[8]} # how long the model is supposed to run
+run_span=${BASH_ARGV[5]} # how long the model is supposed to run
+run_id=${BASH_ARGV[6]} # how long the model is supposed to run
 valgrind_check=0 # set this to 1, if you want to check the code with Valgrind
-start_year=${BASH_ARGV[5]} # defines the start time of the model run
-start_month=${BASH_ARGV[4]} # defines the start time of the model run
-start_day=${BASH_ARGV[3]} # defines the start time of the model run
-start_hour=${BASH_ARGV[2]} # defines the start time of the model run
+start_year=${BASH_ARGV[3]} # defines the start time of the model run
+start_month=${BASH_ARGV[2]} # defines the start time of the model run
+start_day=${BASH_ARGV[1]} # defines the start time of the model run
+start_hour=${BASH_ARGV[0]} # defines the start time of the model run
 
 # grid properties
-orography_id=${BASH_ARGV[9]} # ID of the orography field. Based on this the grid file will be chosen. For ideal_input_id > -1 orography_id will be set automatically.
+orography_id=${BASH_ARGV[7]} # ID of the orography field. Based on this the grid file will be chosen. For ideal_input_id > -1 orography_id will be set automatically.
 
 # dynamics settings
 dt_parameter=1.5 # The sound time step will be calculated as follows: delta_t = dt_parameter*delta_x / km. 1.7 (1.5) can be considered a standard value without (with) RRTMGP.
@@ -63,10 +63,10 @@ pressure_level_output_switch=1 # If set to 1, additional output on pressure_leve
 surface_output_switch=1 # If set to 1, surface variables will be diagnozed and writing to separate files.
 grib_output_switch=1 # If set to 1, output will be written to grib files.
 netcdf_output_switch=0 # If set to 1, output will be written to netcdf files.
-delta_t_between_analyses=${BASH_ARGV[10]} # the time difference between two analyses, only relevant in NWP mode
+delta_t_between_analyses=${BASH_ARGV[8]} # the time difference between two analyses, only relevant in NWP mode
 
 # parallelization
-export OMP_NUM_THREADS=${BASH_ARGV[11]} # relevant for OMP
+export OMP_NUM_THREADS=${BASH_ARGV[9]} # relevant for OMP
 
 # that's it, now the basic run script will be sourced
 source $game_home_dir/run_scripts/.sh/root_script.sh
