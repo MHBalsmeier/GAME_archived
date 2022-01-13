9f23a580e51fa0e96d43cf7cb254bf2ef686b005
Max H. Balsmeier <mhbalsmeier@gmail.com>
2022-01-13 14:24:24 +0100

    grid generator run script updated

--- grid_generator/run_script.sh
+++ grid_generator/run_script.sh
@@ -19,6 +19,5 @@ orography_layers=23 # number of layers following orography (only relevant if typ
 toa=41152 # height of the top of the atmosphere
 type_of_vertical_grid=0 # 0: terrain following coordinates, 1: block-like orography
 valgrind_check=0 # set this to 1 if you want to check the code with Valgrind
-number_of_cpus=1 # relevant only for MPI
 export OMP_NUM_THREADS=6 # relevant only for OMP
 source .sh/run.sh
