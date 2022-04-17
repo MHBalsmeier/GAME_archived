2213a9ab3784f58d202ae6a677ff4a943f6e65c7
Max H. Balsmeier <mhbalsmeier@gmail.com>
2022-04-17 17:23:09 +0200

    grid generator modified

--- grid_generator/src/phys_sfc_properties.c
+++ grid_generator/src/phys_sfc_properties.c
@@ -157,8 +157,8 @@ double sfc_rho_c[], double t_conductivity[], double oro[], int is_land[], int or
 	
 	double c_p_water = 4184.0;
 	double c_p_soil = 830.0;
-	// this is for stability for now
-	c_p_soil = 0.45*c_p_water;
+	// this is the stability limit
+	double c_p_stab_constraints = 0.45*c_p_water*pow(2.0, 5.0 - RES_ID);
 	double albedo_water = 0.06;
 	// setting the land surface albedo to 0.12 (compare Zdunkowski, Trautmann & Bott:
 	// Radiation in the Atmosphere, 2007, p. 444)
@@ -197,7 +197,7 @@ double sfc_rho_c[], double t_conductivity[], double oro[], int is_land[], int or
 				sfc_albedo[i] = albedo_soil;
 			}
 			
-			sfc_rho_c[i] = density_soil*c_p_soil;
+			sfc_rho_c[i] = density_soil*fmax(c_p_soil, c_p_stab_constraints);
 			
 			roughness_length[i] = vegetation_height_ideal(latitude_scalar[i], oro[i])/8.0;
 		}
