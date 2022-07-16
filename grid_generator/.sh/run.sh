3524e7c5ee163543f26b581ee8202ad0681f85f0
Max H. Balsmeier <mhbalsmeier@gmail.com>
2022-07-16 22:57:52 +0200

    grid generation compilation changed

--- grid_generator/.sh/run.sh
+++ grid_generator/.sh/run.sh
@@ -12,13 +12,13 @@ echo "number of Lloyd iterations: $n_iterations"
 
 if [ $use_scalar_h_coords_file -eq 1 ]
 then
-	if [ ! -f $scalar_h_coords_file ]
-	then
-		echo "$scalar_h_coords_file does not exist."
-		echo "Aborting."
-		exit
-	fi
-	echo "Horizontal coordinates of the generating points (the scalar points in terms of the model) will be read from file $scalar_h_coords_file."
+  if [ ! -f $scalar_h_coords_file ]
+  then
+    echo "$scalar_h_coords_file does not exist."
+    echo "Aborting."
+    exit
+  fi
+  echo "Horizontal coordinates of the generating points (the scalar points in terms of the model) will be read from file $scalar_h_coords_file."
 fi
 
 echo "model top: "$toa" m"
@@ -28,32 +28,37 @@ echo "radius rescale factor: "$radius_rescale
 
 if [ $oro_id -eq 1 ]
 then
-	echo "number of points used for averaging the orography: "$no_of_avg_points
+  echo "number of points used for averaging the orography: "$no_of_avg_points
 fi
 
 # downloading orography if necessary
 if [ $oro_id -eq 1 ] && [ ! -f phys_quantities/etopo.nc ]
 then
-	./phys_quantities/download_etopo.sh
+  ./phys_quantities/download_etopo.sh
 fi
 
 if [ $oro_id -eq 1 ] && [ ! -f phys_quantities/B${res_id}_is_land.nc ]
 then
-	echo "Creating land-sea mask ..."
-	python3 .py/is_land.py $res_id
-	echo "Land-sea mask created."
+  echo "Creating land-sea mask ..."
+  python3 .py/is_land.py $res_id
+  echo "Land-sea mask created."
 fi
 
 echo ""
 echo "********** Calling the GAME grid generator **********"
 echo ""
-./grid_generator $oro_id $n_iterations $use_scalar_h_coords_file $scalar_h_coords_file $stretching_parameter $orography_layers $toa $radius_rescale $no_of_avg_points
+./build/grid_generator $oro_id $n_iterations $use_scalar_h_coords_file $scalar_h_coords_file $stretching_parameter $orography_layers $toa $radius_rescale $no_of_avg_points
 if [ $? -ne 0 ]
 then
-	echo -e ${RED}Grid file creation failed.$NC
+  echo -e ${RED}Grid file creation failed.$NC
 else
-	echo "Grid file created sucessfully."
+  echo "Grid file created sucessfully."
 fi
+
+
+
+
+
 
 
 
--- grid_generator/compile.sh
+++ grid_generator/compile.sh
@@ -3,11 +3,14 @@
 # This source file is part of the Geophysical Fluids Modeling Framework (GAME), which is released under the MIT license.
 # Github repository: https://github.com/OpenNWP/GAME
 
-echo "Starting to compile grid generator ..."
-gcc -O2 src/* ../src/constituents/dictionary.c -fopenmp -lnetcdf -lm -Wall -o grid_generator
-if [ $? -ne 0 ]
+if [ ! -d build ]
 then
-	echo -e ${RED}Grid generator compilation failed.$NC
-else
-	echo "Grid generator compiled successfully."
+  mkdir build
 fi
+
+cd build
+
+cmake ..
+make
+
+cd ..
