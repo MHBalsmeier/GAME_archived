d5be270b2ecc6d20320555bfaf9cd04ad5c790d6
Max H. Balsmeier <mhbalsmeier@gmail.com>
2022-07-16 18:15:25 +0200

    tabs in bash scripts

--- plotting/.sh/maps_root.sh
+++ plotting/.sh/maps_root.sh
@@ -5,20 +5,24 @@
 
 if [ ! -d $fig_save_path ]
 then
-mkdir $fig_save_path
+	mkdir $fig_save_path
 fi
+
 echo plotting maps ...
+
 number_of_variables=${#disp_shortname_list[@]}
 for (( i=0; $i<$number_of_variables ; i=$(($i+$omp_num_threads)) ))
 do
 
-for (( proc_id=$i; $proc_id<$(($i+$omp_num_threads)) ; proc_id=$(($proc_id+1)) )) do
-if [ $proc_id -lt $number_of_variables ]
-then
-python3 $game_home_dir/plotting/.py/plot_maps.py $run_span ${plot_intervals_list[$proc_id]} ${disp_level_list[$proc_id]} ${disp_shortname_list[$proc_id]} $fig_save_path $output_dir ${projections_list[$proc_id]} $run_id ${uniform_colormap_list[$proc_id]} ${scope_list[$proc_id]} ${on_pressure_level_list[$proc_id]} ${synoptical_time_mode[$proc_id]} $start_time_since_init &
-fi
-done
-wait
+	for (( proc_id=$i; $proc_id<$(($i+$omp_num_threads)) ; proc_id=$(($proc_id+1)) ))
+	do
+		if [ $proc_id -lt $number_of_variables ]
+		then
+			python3 $game_home_dir/plotting/.py/plot_maps.py $run_span ${plot_intervals_list[$proc_id]} ${disp_level_list[$proc_id]} ${disp_shortname_list[$proc_id]} $fig_save_path $output_dir ${projections_list[$proc_id]} $run_id ${uniform_colormap_list[$proc_id]} ${scope_list[$proc_id]} ${on_pressure_level_list[$proc_id]} ${synoptical_time_mode[$proc_id]} $start_time_since_init &
+		fi
+	done
+	wait
 
 done
+
 echo Finished plotting maps.
--- plotting/.sh/movie_root.sh
+++ plotting/.sh/movie_root.sh
@@ -7,7 +7,7 @@ echo Creating movie ...
 ffmpeg -y -hide_banner -loglevel warning -framerate 50 -i $fig_save_path/$run_id"_"$disp_shortname"_"$disp_level-%00ds.png -c:v libx264 -profile:v high -crf 20 -pix_fmt yuv420p $fig_save_path/$run_id"_"$disp_shortname"_"$disp_level.mp4
 if [ $? -ne 0 ]
 then
-echo -e ${RED}Creaing movie failed.$NC
+	echo -e ${RED}Creaing movie failed.$NC
 else
-echo Movie successfully created.
+	echo Movie successfully created.
 fi
