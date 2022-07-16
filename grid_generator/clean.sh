b97c33a315b3715e13020f2ff5c9ed7d295e2bce
Max H. Balsmeier <mhbalsmeier@gmail.com>
2022-07-16 22:58:31 +0200

    formal change

---
+++ grid_generator/clean.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# This source file is part of the Geophysical Fluids Modeling Framework (GAME), which is released under the MIT license.
+# Github repository: https://github.com/OpenNWP/GAME
+
+if [ -d build ]
+then
+  rm -r build/*
+fi
